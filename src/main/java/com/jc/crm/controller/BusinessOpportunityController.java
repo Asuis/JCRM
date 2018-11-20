6339e1bfdd23e78508772b8967b43e1834ae240e
Asuis <727443530@qq.com>
2018-11-20 23:53:36 +0800

    test auto devp

--- src/main/java/com/jc/crm/controller/BusinessOpportunityController.java
+++ src/main/java/com/jc/crm/controller/BusinessOpportunityController.java
@@ -1,8 +1,9 @@
 package com.jc.crm.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Api
 @RestController
 @RequestMapping("api/busopp")
 public class BusinessOpportunityController {
