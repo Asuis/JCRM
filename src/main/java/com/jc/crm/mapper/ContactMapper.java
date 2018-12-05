088fe8df71afef8bc58a6e9a821f167df6d9f55c
zhanglk <zkk948211@gmail.com>
2018-12-05 23:44:22 +0800

    constat update

--- src/main/java/com/jc/crm/mapper/ContactMapper.java
+++ src/main/java/com/jc/crm/mapper/ContactMapper.java
@@ -51,7 +51,7 @@ public interface ContactMapper {
     ContactsEntity selectContactsName(String contactsName);
 
     /**
-     * 查询客户列表信息 根据客户名称
+     * 查询联系人列表信息 根据客户名称
      * @param contactId 客户名称
      * @return Consumer对象
      * */
@@ -67,7 +67,7 @@ public interface ContactMapper {
      * */
     @Select("<script>" +
             "SELECT * FROM contacts " +
-            "WHERE consumer_id = #{cid}\n " +
+            "WHERE consumer_id = #{cid} AND holder >= 0\n " +
             "    <if test=\"keyword != ''\">\n" +
             "    AND consumer_name LIKE \"%\"#{keyword}\"%\"\n" +
             "    </if>\n" +
@@ -79,9 +79,9 @@ public interface ContactMapper {
     * 删除 联系人
     * @param contactId 联系人id
     * 这里以改变状态 代替删除
-    * holder 作为联系人状态的变化 存在 1 / 删除 -1
+    * holder 作为联系人状态的变化 存在 0 / 删除 -1
      */
     @Delete("update contacts set holder = -1 " +
-            "where consumer_id = #{contactId}")
+            "where contact_id = #{contactId}")
     int deleteById(Integer contactId);
 }
