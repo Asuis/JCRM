b5d5f2029af25a831a2f7e80dfb79b2b5d6ee754
Asuis <727443530@qq.com>
2018-12-10 15:51:15 +0800

    add FileController CommentController

---
+++ src/main/java/com/jc/crm/controller/CommentController.java
@@ -0,0 +1,13 @@
+package com.jc.crm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author asuis
+ * @version: CommentController.java 18-12-10:下午3:49
+ */
+@RestController
+@RequestMapping("/api/comment")
+public class CommentController {
+}
---
+++ src/main/java/com/jc/crm/controller/FileController.java
@@ -0,0 +1,13 @@
+package com.jc.crm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author asuis
+ * @version: FileController.java 18-12-10:下午3:50
+ */
+@RestController
+@RequestMapping("/api/document")
+public class FileController {
+}
